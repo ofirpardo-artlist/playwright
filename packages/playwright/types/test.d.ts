a1cb294231830de0e34af78c82ce0194b1a10460
Ofir Pardo <ofir.p@artlist.io>
2025-05-06 16:36:05 +0300

    add missing override

--- packages/playwright/types/test.d.ts
+++ packages/playwright/types/test.d.ts
@@ -6012,8 +6012,9 @@ export interface TestType<TestArgs extends {}, WorkerArgs extends {}> {
    *
    * Learn more about [fixtures](https://playwright.dev/docs/test-fixtures) and [parametrizing tests](https://playwright.dev/docs/test-parameterize).
    * @param fixtures An object containing fixtures and/or options. Learn more about [fixtures format](https://playwright.dev/docs/test-fixtures).
+   * @param overrides
    */
-  extend<T extends {}, W extends {} = {}>(fixtures: Fixtures<T, W, TestArgs, WorkerArgs>): TestType<TestArgs & T, WorkerArgs & W>;
+  extend<T extends {}, W extends {}, TOverrides extends {}>(fixtures: Fixtures<T, W, TestArgs, WorkerArgs>, overrides: TOverrides): TestType<TestArgs & T, WorkerArgs & W>;
   /**
    * Returns information about the currently running test. This method can only be called during the test execution,
    * otherwise it throws.
